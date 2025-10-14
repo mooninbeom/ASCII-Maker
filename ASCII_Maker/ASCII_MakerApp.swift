931170bb1f37f6190b7c612fca9a6f207099a39d
mooninbeom <dlsqja567@naver.com>
2025-10-14 19:14:57 +0900

    Initial Commit

---
+++ ASCII_Maker/ASCII_MakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASCII_MakerApp.swift
+//  ASCII_Maker
+//
+//  Created by 문인범 on 10/14/25.
+//
+
+import SwiftUI
+
+@main
+struct ASCII_MakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
