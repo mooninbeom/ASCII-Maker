5ddb5ed0118990c2afd4e34cf534fc39c5d9f978
mooninbeom <dlsqja567@naver.com>
2025-10-17 13:20:56 +0900

    [#3] 메인 뷰 러프 디자인

--- ASCII_Maker/Presents/MainView.swift
+++ ASCII_Maker/Presents/MainView.swift
@@ -11,13 +11,9 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        
         ZStack {
             Color.init(hex: "#090B30")
                 .ignoresSafeArea()
-            
-            
-            
             
             VStack {
                 Button {
@@ -31,14 +27,105 @@ struct MainView: View {
                 Image(.mainLogo)
                 
                 Text("내 이미지를 손쉽게 ASCII ART로!")
+                    .koreanFont(size: 15)
+                    .customColor(.secondary)
                 
-                Text("ASCII ART란?")
+                HStack {
+                    Text("ASCII ART란?")
+                        .koreanFont(size: 15)
+                        .customColor(.secondary)
+                    
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "info.circle")
+                            .font(.system(size: 10))
+                    }
+                    .foregroundStyle(.gray)
+                }
+                
+                
+                Button {
+                    
+                } label: {
+                    VStack {
+                        Text("이미지 선택하기")
+                            .koreanFont(size: 15)
+                        
+                        Image(systemName: "photo")
+                            .font(.system(size: 100))
+                    }
+                }
+                .foregroundStyle(.white)
+                
+                Spacer()
+                HStack {
+                    VStack {
+                        Text("변환 품질")
+                            .koreanFont(size: 25)
+                            .customColor(.text)
+                        
+                        qualityButton
+                        qualityButton
+                        qualityButton
+                        qualityButton
+                    }
+                    
+                    Spacer()
+                }
                 
                 Spacer()
                 
+                Button {
+                    
+                } label: {
+                    ZStack {
+                        Rectangle()
+                            .frame(height: 80)
+                            .customColor(.primary)
+                        
+                        
+                        HStack(spacing: 20) {
+                            Image(systemName: "photo")
+                                .font(.system(size: 30))
+                                .foregroundStyle(.white)
+                            
+                            Image(systemName: "arrowshape.right.fill")
+                                .font(.system(size: 18))
+                                .foregroundStyle(.white)
+                            
+                            Image(systemName: "text.quote")
+                                .font(.system(size: 30))
+                                .foregroundStyle(.white)
+
+                        }
+                    }
+                }
+                
+
+                    
+                
             }
+            .padding(.horizontal, 30)
         }
 
+    }
+    
+    private var qualityButton: some View {
+        HStack {
+            Circle()
+                .frame(width: 20, height: 20)
+                .foregroundStyle(.white)
+                .overlay {
+                    Circle()
+                        .frame(width: 14, height: 14)
+                        .customColor(.primary)
+                }
+            
+            Text("원본 화질")
+                .koreanFont(size: 14)
+                .foregroundStyle(.white)
+        }
     }
 }
 
